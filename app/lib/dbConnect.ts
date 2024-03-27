5b02c8e8fa9cd97557572dab7b9cc1839dea8a2c
unknown <sandeepduhan1432@gmail.com>
2024-03-27 08:14:08 -0700

    db

--- app/lib/dbConnect.ts
+++ app/lib/dbConnect.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 async function dbConnect() {
     try{
-        await mongoose.connect("mongodb://localhost:27017/ECOMM")
+        await mongoose.connect("mongodb+srv://sandeepduhan1432:sandeepduhan1432@cluster0.xdjbkvl.mongodb.net/ECOMM")
     }
     catch(error){
         throw new Error("Connection faild")
